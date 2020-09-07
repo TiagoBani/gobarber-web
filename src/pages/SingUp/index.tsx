039f8fae0d85d41499075b100d00106a54763365
tiagobani <tiago.bani@yahoo.com.br>
2020-09-06 21:46:34 -0300

    [feat]: add page singup

---
+++ src/pages/SingUp/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Background, Container, Content } from './styles';
+
+const SingUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="Gobarber" />
+        <form>
+          <h1>Faça seu logon</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para Logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SingUp;
