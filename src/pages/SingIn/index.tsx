90b6235eabddc3606dbe1a64aba857d59e22a7e4
tiagobani <tiago.bani@yahoo.com.br>
2020-09-07 18:38:22 -0300

    add customHook of authContext

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Form } from '@unform/web';
@@ -7,7 +7,7 @@ import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -23,7 +23,7 @@ interface SingInFormData {
 const SingIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const { signIn } = useAuth();
 
   const handleSubmit = useCallback(
     async (data: SingInFormData) => {
