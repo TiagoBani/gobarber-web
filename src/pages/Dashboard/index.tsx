ad3797eeb3b708601456b7466882850ac785893c
tiagobani <tiago.bani@yahoo.com.br>
2020-10-11 19:24:14 -0300

    [feat]: add header in Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { FiPower } from 'react-icons/fi';
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+import { useAuth } from '../../hooks/auth';
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
+  const { singOut, user } = useAuth();
+
   return (
     <Container>
-      <h1>Dashboard</h1>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={singOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 };
