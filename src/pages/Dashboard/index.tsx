87bbd0ccac9b292ee9e529d4cf9049de31a24bb7
tiagobani <tiago.bani@yahoo.com.br>
2020-10-11 21:13:51 -0300

    [feat]: add dashboard page with static content

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 
 import { useAuth } from '../../hooks/auth';
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
   const { singOut, user } = useAuth();
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDayChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
 
   return (
     <Container>
@@ -29,6 +52,108 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/3676633?s=460&u=ded5bb6cb58240237e19c9ab4f5ed5a2c57dee29&v=4"
+                alt="Tiago Bani"
+              />
+
+              <strong>Tiago Bani</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/3676633?s=460&u=ded5bb6cb58240237e19c9ab4f5ed5a2c57dee29&v=4"
+                  alt="Tiago Bani"
+                />
+
+                <strong>Tiago Bani</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                10:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/3676633?s=460&u=ded5bb6cb58240237e19c9ab4f5ed5a2c57dee29&v=4"
+                  alt="Tiago Bani"
+                />
+
+                <strong>Tiago Bani</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                13:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/3676633?s=460&u=ded5bb6cb58240237e19c9ab4f5ed5a2c57dee29&v=4"
+                  alt="Tiago Bani"
+                />
+
+                <strong>Tiago Bani</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            selectedDays={selectedDate}
+            modifiers={{ available: { daysOfWeek: [1, 2, 3, 4, 5] } }}
+            onDayClick={handleDayChange}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calendar>
+      </Content>
     </Container>
   );
 };
