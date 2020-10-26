635418c0fe38899f4e8a3432d987d7a467f95406
tiagobani <tiago.bani@yahoo.com.br>
2020-10-25 21:54:50 -0300

    [test]: add mocks in signin

--- src/__tests__/pages/SingIn.spec.tsx
+++ src/__tests__/pages/SingIn.spec.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, wait } from '@testing-library/react';
 import SingIn from '../../pages/SingIn';
 
+const mockedAddToast = jest.fn();
+const mockedHistoryPush = jest.fn();
+
+jest.mock('../../hooks/auth', () => {
+  return {
+    useAuth: () => ({ signIn: jest.fn() }),
+  };
+});
+
+jest.mock('../../hooks/toast', () => {
+  return {
+    useToast: () => ({ addToast: mockedAddToast }),
+  };
+});
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({ push: mockedHistoryPush }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('SignIn Page', () => {
-  it('should be able to sign in', () => {
-    const { debug } = render(<SingIn />);
+  it('should be able to sign in', async () => {
+    const { getByPlaceholderText, getByText } = render(<SingIn />);
 
-    debug();
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123123' } });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedAddToast).toHaveBeenCalledWith({
+        title: 'Você agora está logado.',
+        type: 'info',
+      });
+    });
   });
 });
