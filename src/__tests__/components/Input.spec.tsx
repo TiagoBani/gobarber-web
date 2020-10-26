fccdc3b4efe4b84799f058f0dcef735a1cf66d7e
tiagobani <tiago.bani@yahoo.com.br>
2020-10-26 00:26:58 -0300

    [test]: add focus input

---
+++ src/__tests__/components/Input.spec.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import 'jest-styled-components';
+import { render, fireEvent, wait } from '@testing-library/react';
+import Input from '../../components/Input';
+
+jest.mock('@unform/core', () => {
+  return {
+    useField() {
+      return {
+        fieldName: 'name',
+        defaultValue: '',
+        error: '',
+        registerField: jest.fn(),
+      };
+    },
+  };
+});
+
+describe('Input component', () => {
+  it('should be able to render an input', () => {
+    const { getByPlaceholderText } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    expect(getByPlaceholderText('E-mail')).toBeTruthy();
+  });
+
+  it('should render highlight on input focus', async () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    const inputElement = getByPlaceholderText('E-mail');
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.focus(inputElement);
+
+    await wait(() => {
+      expect(containerElement).toHaveStyleRule('border-color', '#ff9000');
+      expect(containerElement).toHaveStyleRule('color', '#ff9000');
+    });
+  });
+});
